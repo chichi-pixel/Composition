35e2ee9b90f528493308648baffbbaf2384b32d7
chichi-pixel <60243643+chichi-pixel@users.noreply.github.com>
2022-03-14 08:02:34 +0100

    Composition

---
+++ Composition/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var Installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+
+            Installer.Install();
+
+        }
+    }
+}
